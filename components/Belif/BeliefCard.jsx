83d03c58d31eee7f9e6319ecd69dd6744369b54f
allbright <rofiqafolabi@gmail.com>
2026-04-17 16:31:12 +0100

    tsx back to jsx

--- components/Belif/BeliefCard.jsx
+++ components/Belif/BeliefCard.jsx
@@ -1,9 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const BeliefCard = ({ data = [] }) => {
+const BeliefCard = ({ data }) => {
+  const beliefs = data || [];
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 p-5 lg:p-20 sm:p-5 md:p-5">
-      {data.map((belief) => (
+      {beliefs.map((belief) => (
         <div
           key={belief.id}
           className="bg-white rounded-2xl shadow-[0_0_20px_rgba(0,0,0,0.1)] hover:shadow-[0_0_30px_rgba(0,0,0,0.15)] hover:-translate-y-1 transition duration-300 p-8 flex flex-col items-center justify-center text-center gap-4"

0337497229ed7a1334b17a689b21a2cf672b50d6
allbright <rofiqafolabi@gmail.com>
2026-05-01 02:27:23 +0100

    last changes

---
+++ app/not-found.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import Link from 'next/link';
+const notFound = () => {
+  return (
+    <>
+  <Navbar variant = "white" />
+ <div className="flex flex-col gap-4 lg:py-30 py-10 max-w-2xl text-center mx-auto items-center">
+    <h3 className="text-xl font-bold lg:text-2xl text-black">404</h3>
+
+    <p className="text-black text-xl">Sorry we couldnt find the page your were looking for.</p>
+       <Link href="/" ><button className="bg-[#8d8d8d] cursor-pointer font-bold px-5 py-3 text-white w-fit transition rounded-3xl">
+     Go Back Home
+    </button></Link>
+  </div>
+  <Footer/>
+  </>
+  )
+}
+
+export default notFound
